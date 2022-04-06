c57751dd96173ea25282d9fe03c7f44295767fc9
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2022-04-05 23:26:12 -0700

    timer update
    added timer functionality, blinking light, fixed resets

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ const keyRelease = new Audio("../audio/key-release.mp3");
 const btnStart = document.getElementById('btn-start');
 const overlay = document.querySelector('.overlay');
 const main = document.querySelector('main');
+const light = document.querySelector('.light');
+
 
 //////////-- Start Game Overlay --//////////
 
@@ -15,6 +17,7 @@ btnStart.addEventListener('click', () => {
   overlay.style.display = 'none';
   main.style.display = 'block';
   wordsToFlex();
+  startTimer();
 });
 
 //////////-- Random Phrase Selector --//////////
@@ -25,6 +28,10 @@ const phrases = [
   "Programming isn't about what you know; it's about what you can figure out.",
   'The best error message is the one that never shows up.',
   'First, solve the problem. Then, write the code.',
+  'Fix the cause, not the symptom.',
+  'Make it work, make it right, make it fast.',
+  'Optimism is an occupational hazard of programming.',
+  'Java is to JavaScript what car is to Carpet.',
 ];
 
 /* retrieves random phrase and stops phrases from being selected twice in a row */
@@ -37,7 +44,7 @@ function getRandomPhrase () {
 
   if (randomNum !== previousNum) {
     selectedPhrase = `${phrases[randomNum]}`;
-    console.log(selectedPhrase);
+    // console.log(selectedPhrase);
     previousNum = randomNum;
   } else {
     getRandomPhrase();
@@ -53,7 +60,7 @@ let words = [];
 function splitPhrase() {
   let phrase = getRandomPhrase();
   words = phrase.split(' ');
-  console.log(words);
+  // console.log(words);
 }
 
 const phraseContainer = document.querySelector('.phrase-container');
@@ -84,7 +91,7 @@ function wordsToFlex() {
       // div.textContent = text; //change out
       flex.appendChild(div);
     }
-    console.log(flex);
+    // console.log(flex);
     phraseContainer.appendChild(flex);
   });
 }
@@ -108,6 +115,141 @@ keyboard.addEventListener('mousedown', (e) => {
     });
   }
 });
+
+
+
+///////////////////////////////////////////////////////////
+//                          Timer                        //    
+///////////////////////////////////////////////////////////
+/*
+const startingMinutes = 1;       // Starting time in minutes
+let seconds = startingMinutes * 60;  // Calculates starting time in secs
+
+const countdownEl = document.getElementById('countdown');
+const tickSound1 = new Audio('../audio/tick1.mp3');
+const tickSound2 = new Audio('../audio/tick2.mp3');
+
+function startTimer() { // Allows user to start timer when ready
+  setInterval(updateCountdown, 1000); // setInterval(functionName, interval in ms)
+}
+
+function updateCountdown() {
+  const displayMinutes =  Math.floor(seconds / 60); 
+  let displaySeconds = seconds % 60;
+
+  if (displaySeconds < 10) {
+    displaySeconds = `0${displaySeconds}`;
+  }
+
+  if (displaySeconds % 2 == 0) { 
+    tickSound1.play();
+    console.log('tick 1')
+  } else {
+    tickSound2.play();
+    console.log('tick 2')
+  }
+
+
+  countdownEl.innerHTML = `${displayMinutes}:${displaySeconds}`;
+  seconds--;
+}
+*/
+////////////////////////////////////////////////////////////////////////
+
+const tickSound2 = new Audio('../audio/tick1.1.mp3');
+const tickSound1 = new Audio('../audio/tick2.1.mp3');
+const explosionSound = new Audio('../audio/explosion.mp3');
+
+const minutes = 0.5;
+let seconds = Math.floor(minutes * 60);
+originalSeconds = seconds;
+let quarterSecond = seconds * 4;
+let originalTime = quarterSecond;
+let timerID;
+let tick; 
+
+const countdownEl = document.getElementById('countdown');
+
+function startTimer() {
+  timerID = setInterval(timer, 250);
+}
+
+
+function timer() {
+
+  if (quarterSecond <= 20) {
+    if (tick) {
+      tickSound1.play();
+      lightToggle();
+      tick = 0;
+    } else {
+      tickSound2.play();
+      lightToggle();
+      tick = 1;
+    }
+
+  } else if (quarterSecond > 20 && quarterSecond <= 60 && quarterSecond % 2 == 0) {
+        if (tick) {
+          tickSound1.play();
+          lightToggle();
+          tick = 0;
+        } else {
+          tickSound2.play();
+          lightToggle();
+          tick = 1;
+        }
+
+  } else if (quarterSecond % 4 == 0) {
+      if (tick) {
+        tickSound1.play();
+        lightToggle();
+        tick = 0;
+      } else {
+        tickSound2.play();
+        lightToggle();
+        tick = 1;
+      }
+  }
+  
+  quarterSecond--;
+  console.log(quarterSecond);
+  
+  if (quarterSecond == -5) {
+    endGame();
+  }
+
+  if (quarterSecond % 4 == 0 ) { 
+    seconds--;
+  }
+
+  if (quarterSecond < 20) {
+    countdownEl.style.color = 'red';
+  }
+
+  let displayMinutes =  Math.floor(seconds / 60); 
+  let displaySeconds = seconds % 60;
+
+    if (displaySeconds < 10 && displaySeconds > 0) {
+      displaySeconds = `0${displaySeconds}`;
+    } else if (displaySeconds <= 0) {
+      displaySeconds = '00';
+      displayMinutes = '0';
+    }
+  
+  countdownEl.innerHTML = `${displayMinutes}:${displaySeconds}`;
+}
+
+
+/////////-- Toggle Light --//////////
+
+function lightToggle() {
+  light.className += ' light-on'; 
+  setTimeout( () => {
+    light.className = 'light'
+  }, 150 );
+}
+
+////////////////////////////////////////////////////////////////////////
 
 //////////-- Input Checks --//////////
 
@@ -161,13 +303,17 @@ keyboard.addEventListener('click', (e) => {
     console.log(`Health: ${health}`);
   }
 
-  if (health == 0) {
+  if (health <= 0) {
     endGame();
+    
   }
+
 });
 
 
 //////////-- EndGame Overlay (Lost) --//////////
+
+
 
 function endGame() {
   const overlayText = document.querySelector('p');
@@ -176,12 +322,17 @@ function endGame() {
   overlay.style.display = 'flex';
   main.style.display = 'none';
 
-  overlayText.textContent = 'Bummer! Next time try to cut the red wire... Oh wait... my bad. Wrong game.';
+  overlayText.textContent = 'BOOM! Next time try to cut the red wire... Oh wait... my bad. Wrong game.';
   resetBtn.value = 'Reset';
   overlay.className += ' end-game';
 
   phraseContainer.innerHTML = '';
   health = 5;
-}
+  seconds = originalSeconds;
+  quarterSecond = originalTime;
 
+  clearInterval(timerID);
+  explosionSound.currentTime = 0;
+  explosionSound.play();
+}
 
