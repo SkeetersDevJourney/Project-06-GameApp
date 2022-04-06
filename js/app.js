4de8d14b2eb4bfbec6592a423812dec99b1fce8f
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2022-04-05 21:45:25 -0700

    update
    displaying correct letters, tracking health, lose overlay, reset

--- js/app.js
+++ js/app.js
@@ -14,8 +14,80 @@ const main = document.querySelector('main');
 btnStart.addEventListener('click', () => {
   overlay.style.display = 'none';
   main.style.display = 'block';
-  phraseToLi();
+  wordsToFlex();
 });
+
+//////////-- Random Phrase Selector --//////////
+
+const phrases = [
+  'Javascript is the duct tape of the Internet.',
+  'Simplicity is the soul of efficiency.',
+  "Programming isn't about what you know; it's about what you can figure out.",
+  'The best error message is the one that never shows up.',
+  'First, solve the problem. Then, write the code.',
+];
+
+/* retrieves random phrase and stops phrases from being selected twice in a row */
+
+let selectedPhrase;
+let previousNum;
+function getRandomPhrase () {
+
+  let randomNum = Math.floor(Math.random() * 5);
+
+  if (randomNum !== previousNum) {
+    selectedPhrase = `${phrases[randomNum]}`;
+    console.log(selectedPhrase);
+    previousNum = randomNum;
+  } else {
+    getRandomPhrase();
+  }
+  return selectedPhrase;
+};
+
+
+//////////-- Create Phrase Markup --//////////
+
+let words = [];
+
+function splitPhrase() {
+  let phrase = getRandomPhrase();
+  words = phrase.split(' ');
+  console.log(words);
+}
+
+const phraseContainer = document.querySelector('.phrase-container');
+
+function createWordFlex() {
+  let flex = document.createElement('div');
+  flex.className = 'word';
+  return flex;
+}
+
+function wordsToFlex() {
+  splitPhrase();
+  words.forEach(word => {
+    let flex = createWordFlex();
+    for (let i=0; i<word.length; i++) {
+      text = word[i].toUpperCase();
+      div = document.createElement('div');
+      div.className = 'letter';
+      if (text == ';' 
+          || text == '.'
+          || text == "'"
+          || text == ",") {
+        div.textContent = text;
+        div.style.border = 'none';
+      } else {
+        div.textContent = ' ';
+      }
+      // div.textContent = text; //change out
+      flex.appendChild(div);
+    }
+    console.log(flex);
+    phraseContainer.appendChild(flex);
+  });
+}
 
 //////////-- Keyboard Clicking Events --//////////
 
@@ -37,58 +109,79 @@ keyboard.addEventListener('mousedown', (e) => {
   }
 });
 
-//////////-- Random Phrase Selector --//////////
+//////////-- Input Checks --//////////
 
-const phrases = [
-  'Javascript rules!',
-  'Treehouse Techdegree',
-  'Front-End Web Dev',
-  'Arrays are easy.',
-  'Hello from app.js',
-];
+// keyboard.addEventListener('click', (e) => {
 
-/* This function stops phrases from being selected twice in a row */
+//   const key = e.target;
+//   const matchDiv = document.getElementsByClassName('letter');
+//   if (key.tagName === 'INPUT') {
+//     words.forEach(word => {
+//       for (let i=0; i<word.length; i++) {
+//         if (word[i].toUpperCase() == userInput) {
+//           const matchIndex = word.indexOf(word[i]);
+//           console.log(matchIndex);
 
-let selectedPhrase;
-let previousNum;
-function getRandomPhrase () {
 
-  let randomNum = Math.floor(Math.random() * 5);
+//         } else {
+//         }
+//       }
+//     });
+// }
+// })
 
-  if (randomNum !== previousNum) {
-    selectedPhrase = `${phrases[randomNum]}`;
-    console.log(selectedPhrase);
-    previousNum = randomNum;
-  } else {
-    getRandomPhrase();
+// function throughWords() {
+//   words.forEach(word => {
+//     for (let i=0; i<word.length; i++)
+//     console.log(word.indexOf(word[i]));
+//   });
+// }
+
+let health = 5;
+
+keyboard.addEventListener('click', (e) => {
+  let allLetters = selectedPhrase.replace(/ /g, '').toUpperCase();
+  const found = userInput.match(allLetters);
+  let check = true;
+
+  console.log(allLetters);
+
+  for (let i=0; i<allLetters.length; i++) {
+    let allHTMLLetters = document.getElementsByClassName('letter');
+    if (userInput == allLetters[i]) {
+      injectLetter = allLetters[i];
+      letterIndex = allHTMLLetters[i];
+      letterIndex.textContent = injectLetter;
+      check = false;
+    } 
   }
-};
 
-
-//////////-- Create Phrase Markup --//////////
-
-const phraseContainer = document.querySelector('.phrase-container');
-
-function phraseToLi() {
-  getRandomPhrase();
-  for (let i=0; i<selectedPhrase.length; i++) {
-    let text = selectedPhrase[i];
-    createLiEl(text);
+  if (check == true) {
+    health--;
+    console.log(`Health: ${health}`);
   }
-}
 
-function createLiEl(text) {
-  let li = document.createElement('li');
-  li.className = 'letter';
-  li.textContent = text;
-  if (text == ' ' 
-      || text == '!' 
-      || text == '.'
-      || text == '-') {
-    li.style.borderBottom = 'none';
+  if (health == 0) {
+    endGame();
   }
-  phraseContainer.appendChild(li);
-  console.log(phraseContainer.appendChild(li));
+});
+
+
+//////////-- EndGame Overlay (Lost) --//////////
+
+function endGame() {
+  const overlayText = document.querySelector('p');
+  const resetBtn = document.getElementById('btn-start');
+
+  overlay.style.display = 'flex';
+  main.style.display = 'none';
+
+  overlayText.textContent = 'Bummer! Next time try to cut the red wire... Oh wait... my bad. Wrong game.';
+  resetBtn.value = 'Reset';
+  overlay.className += ' end-game';
+
+  phraseContainer.innerHTML = '';
+  health = 5;
 }
 
 
